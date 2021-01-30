60537f8f12d98c8550674146898188905f0c9ba4
vagrant <vagrant@homestead>
2021-01-30 15:38:18 +0000

    authRequest and logout

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,8 +1,24 @@
 import { request } from '@/utils/request'
 
+export function refresh(token) {
+  return request('authorizations/current', {
+    method: 'put',
+    header: {
+      'Authorization': 'Bearer ' + token
+    }
+  })
+}
 export function login(data) {
   return request('weapp/authorizations', {
     method: 'post',
     data: data
+  })
+}
+export function logout(token) {
+  return request('authorizations/current', {
+    method: 'delete',
+    header: {
+      'Authorization': 'Bearer ' + token
+    }
   })
 }
