1255b20bd76fd11dbb9a1739167d4f3f7dfc8cdc
vagrant <vagrant@homestead>
2021-01-31 08:13:14 +0000

    page user

--- src/store/index.js
+++ src/store/index.js
@@ -7,29 +7,3 @@ export default new Vuex.Store({
   }
 })
 
-// export default new Vuex.Store({
-//   state: {
-//     counter: 0
-//   },
-//   mutations: {
-//     increment (state) {
-//       state.counter++
-//     },
-//     decrement (state) {
-//       state.counter--
-//     }
-//   },
-//   actions: {
-//     increment ({ commit }) {
-//       commit('increment')
-//     },
-//     decrement ({ commit }) {
-//       commit('decrement')
-//     },
-//     incrementAsync ({ commit }) {
-//       setTimeout(() => {
-//         commit('increment')
-//       }, 1000)
-//     }
-//   }
-// })
